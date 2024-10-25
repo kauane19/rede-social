c8b1b420db6c1bf25c23253222fd4b676e1c5d71
kauane19 <105223810+kauane19@users.noreply.github.com>
2024-10-25 09:36:55 -0300

    kkkkkk

--- common.js
+++ common.js
@@ -1,4 +1,12 @@
-</footer>
-<script type="module" src="graficos/common.js"></script>
-<script type="module" src="graficos/informacoesGlobais.js"></script>
-<script type="module" src="graficos/quantidadeUsuarios.js"></script>
+const getCSS = (variavel) => {
+    const bodyStyles = getComputedStyle(document.body)
+    return bodyStyles.getPropertyValue(variavel)
+}
+
+const tickConfig = {
+    family: getCSS('--font'),
+    size: 16,
+    color: getCSS('--primary-color')
+}
+
+export {getCSS, tickConfig}
