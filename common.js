e8be4b889dd62ef323d5a252a3fadd471babb8e1
kauane19 <105223810+kauane19@users.noreply.github.com>
2024-10-25 09:30:10 -0300

    .

---
+++ common.js
@@ -0,0 +1,4 @@
+</footer>
+<script type="module" src="graficos/common.js"></script>
+<script type="module" src="graficos/informacoesGlobais.js"></script>
+<script type="module" src="graficos/quantidadeUsuarios.js"></script>
